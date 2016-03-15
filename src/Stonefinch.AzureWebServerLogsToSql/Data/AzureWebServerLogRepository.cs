d48cd54fa2db92734d345413b3c6bab7c7846d46
Aaron Hoffman <hoffman.aaron.d@gmail.com>
2016-03-15 14:09:35 -0500

    Use fileNameAndPath instead of just filename

--- src/Stonefinch.AzureWebServerLogsToSql/Data/AzureWebServerLogRepository.cs
+++ src/Stonefinch.AzureWebServerLogsToSql/Data/AzureWebServerLogRepository.cs
@@ -28,27 +28,28 @@ namespace Stonefinch.AzureWebServerLogsToSql.Data
             using (var ctx = this.CreateAzureWebServerLogContext())
             {
                 // filesInfos should have unique fileNames
-                var fileInfosDict = fileInfos.ToDictionary(x => x.FileName);
+                var fileInfosDict = fileInfos.ToDictionary(x => x.FileNameAndPath);
 
                 // get records from DB with info on the last time these log files were processed
                 var fileInfosFromDb =
                     (from fi in ctx.AzureWebServerLogFileInfos
-                     where fileInfosDict.Keys.Contains(fi.FileName)
-                     select fi).ToDictionary(x => x.FileName);
+                     where fileInfosDict.Keys.Contains(fi.FileNameAndPath)
+                     select fi).ToDictionary(x => x.FileNameAndPath);
 
                 // determine log files that are new, or that have changed since the last time we processed them
                 foreach (var fileInfo in fileInfosDict.Values)
                 {
                     // if the file already existed in the DB, determine if it has changed.
-                    if (fileInfosFromDb.ContainsKey(fileInfo.FileName))
+                    if (fileInfosFromDb.ContainsKey(fileInfo.FileNameAndPath))
                     {
-                        var fileInfoFromDb = fileInfosFromDb[fileInfo.FileName];
+                        var fileInfoFromDb = fileInfosFromDb[fileInfo.FileNameAndPath];
 
                         // determine if the file provided has changes from the record we have
                         if (fileInfoFromDb.FileSize != fileInfo.FileSize
                             || fileInfoFromDb.LastModifiedDate != fileInfo.LastModifiedDate)
                         {
-                            // changes identified, add to result set
+                            // changes identified, add to result set (set primary key from DB)
+                            fileInfo.AzureWebServerLogFileInfoId = fileInfoFromDb.AzureWebServerLogFileInfoId;
                             result.Add(fileInfo);
                         }
                         else
